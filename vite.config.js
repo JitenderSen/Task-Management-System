e7c0449f7b79d9474e80264033d70c14a1503dda
JitenderSen <jitendersen239@gmail.com>
2025-06-06 11:56:37 +0530

    Added homepage and base config for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Task-Management-System/', // 👈 Ye repo name hai (case-sensitive)
 })
